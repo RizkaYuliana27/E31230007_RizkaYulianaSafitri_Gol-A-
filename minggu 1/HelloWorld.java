802cf5770b532bd6ff9aac3e50ff638599b750d4
E31230007_RizkaYulianaSafitri_Gol A <158010453+RizkaYuliana27@users.noreply.github.com>
2024-01-30 09:07:50 +0700

    Create HelloWorld.java

---
+++ minggu 1/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package koreksi;
+
+/**
+ *
+ * @author Rizka
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        // TODO code application logic here
+        System.out.println("Hello World..!");
+    }
+    
+}
